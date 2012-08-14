8456d4dc098c7d8596a10b9cd399eadc81c47cc8
Martin Naumann <martin.naumann@centralway.com>
2012-08-14 17:42:30 +0200

    Spicing up the app.js with error handling, static content

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var Todo  = require("./todo.js");
 var express = require("express");
     
 var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(express.errorHandler());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 var todoItems = [Todo("A"), Todo("B", true)];
 
 app.get("/", function index(req,resp) {
-    console.log(todoItems);   
     resp.render("index",{todos: todoItems});
 });
 
